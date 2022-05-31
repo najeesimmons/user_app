48159faf08d56bbeb9f6d8772bb3e914e426d76e
najeesimmons <najee.bryant.simmons@gmail.com>
2022-05-31 13:30:26 -0400

    built out UsersList with map function

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,7 +1,18 @@
-const UsersList = () => {
-    return <ul>
-        
-    </ul>
-}
+import Card from "../UI/Card";
+import style from "./UsersList.module.css";
 
-export default UsersList
+const UsersList = (props) => {
+  return (
+    <Card classname={style.users}>
+      <ul>
+        {props.users.map(user => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
