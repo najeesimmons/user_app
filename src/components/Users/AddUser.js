01f8b85decbea294af48aa7f0e52a4171e6c3e5e
najeesimmons <najee.bryant.simmons@gmail.com>
2022-06-06 16:43:16 -0400

    replaced state with useRef for managing input fields in AddUser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,11 +4,9 @@ import Button from "../UI/Button";
 import { useState, useRef } from "react";
 import ErrorModal from "../UI/ErrorModal";
 
-
 const AddUser = (props) => {
   const nameInputRef = useRef();
   const ageInputRef = useRef();
-
 
   const [error, setError] = useState();
 
@@ -18,44 +16,42 @@ const AddUser = (props) => {
     const enteredAge = ageInputRef.current.value;
     if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
-        title: 'Invalid Input',
-        message: 'Please enter a valid name and age (non empty values).'
+        title: "Invalid Input",
+        message: "Please enter a valid name and age (non empty values).",
       });
       return;
     }
     if (+enteredAge < 1) {
       setError({
-        title: 'Invalid age',
-        message: 'Please enter a age (> 0).'
+        title: "Invalid age",
+        message: "Please enter a age (> 0).",
       });
       return;
     }
     props.onAddUser(enteredName, enteredAge);
-    nameInputRef.current.value = ""
-    ageInputRef.current.value = ""
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
-    setError(null)
+    setError(null);
   };
 
   return (
     <>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={style.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            ref={nameInputRef}
-          ></input>
+          <input id="username" type="text" ref={nameInputRef}></input>
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="text"
-            ref={ageInputRef}
-          ></input>
+          <input id="age" type="text" ref={ageInputRef}></input>
           <Button type="submit">Add User</Button>
         </form>
       </Card>
@@ -64,4 +60,3 @@ const AddUser = (props) => {
 };
 
 export default AddUser;
-
