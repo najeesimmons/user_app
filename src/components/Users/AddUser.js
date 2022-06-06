fb5de3597e003898ee9b5571964f684f6639ed41
najeesimmons <najee.bryant.simmons@gmail.com>
2022-06-06 11:22:00 -0400

    replaced Wrapper component with React Fragment

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import style from "./AddUser.module.css";
 import Button from "../UI/Button";
 import { useState } from "react";
 import ErrorModal from "../UI/ErrorModal";
-import Wrapper from "../Helpers/Wrapper";
+
 
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
@@ -43,7 +43,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <Wrapper>
+    <>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <Card className={style.input}>
         <form onSubmit={addUserHandler}>
@@ -64,7 +64,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </Wrapper>
+    </>
   );
 };
 
